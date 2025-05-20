ae55515d9c78703468b3d13132297c4d598f1e7a
midota-lamhv <lamhv@midota.com>
2025-05-20 18:05:13 +0700

    update:user page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="py-6">
     <dashbroad />
-    <adminUser />
   </div>
 </template>
 
 <script lang="ts" setup>
 import dashbroad from './component/dashbroad.vue'
-import adminUser from './user/index.vue'
 definePageMeta({
   layout: 'adminlayout'
 })
---
+++ pages/admin/user/userdetail.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Đây là trang userdetail</div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'adminlayout'
+})
+</script>
+
+<style scoped></style>
